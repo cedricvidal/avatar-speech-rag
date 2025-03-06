f1cb1fdd74d9169f06c661ffc1550e278303a53d
Cedric Vidal <cedric@vidal.biz>
2025-03-06 00:39:19 +0000

    Hi animation

--- app/frontend/src/Avatar.tsx
+++ app/frontend/src/Avatar.tsx
@@ -1,7 +1,8 @@
 import { Avatar } from "@readyplayerme/visage";
+import hi_animation from "./assets/Hi_animation.fbx";
 
 const modelSrc = "https://readyplayerme.github.io/visage/male.glb";
 
 export function RTAvatar() {
-    return <Avatar modelSrc={modelSrc} />;
+    return <Avatar modelSrc={modelSrc} animationSrc={hi_animation} />;
 }
