b5d88dae7c2f0279d1f78b59d5c52014aa59f9b5
Cedric Vidal <cedric@vidal.biz>
2025-03-06 19:39:52 +0000

    Lower level avatar management

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -15,7 +15,7 @@ import { GroundingFile, ToolResult } from "./types";
 
 import logo from "./assets/logo.svg";
 
-import { RTAvatar } from "./components/avatar/Avatar";
+import { AvatarCanvas } from "./components/avatar/Avatar";
 import { weatherTool } from "@/tools/weather";
 import { hiTool } from "@/tools/hi";
 
@@ -79,15 +79,15 @@ function App() {
     const { t } = useTranslation();
 
     return (
-        <div className="flex min-h-screen flex-col bg-gray-100 text-gray-900">
+        <div className="App">
             <div className="p-4 sm:absolute sm:left-4 sm:top-4">
                 <img src={logo} alt="Azure logo" className="h-16 w-16" />
             </div>
-            <main className="flex flex-grow flex-col items-center justify-center">
+            <main className="h-screen w-full">
                 <h1 className="mb-8 bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-4xl font-bold text-transparent md:text-7xl">
                     {t("app.title")}
                 </h1>
-                <RTAvatar activeAnimation={activeAnimation} />
+                <AvatarCanvas activeAnimation={activeAnimation} />
                 <div className="mb-4 flex flex-col items-center justify-center">
                     <Button
                         onClick={onToggleListening}
@@ -107,14 +107,11 @@ function App() {
                     </Button>
                     <StatusMessage isRecording={isRecording} />
                 </div>
-                <GroundingFiles files={groundingFiles} onSelected={setSelectedFile} />
             </main>
 
             <footer className="py-4 text-center">
                 <p>{t("app.footer")}</p>
             </footer>
-
-            <GroundingFileView groundingFile={selectedFile} onClosed={() => setSelectedFile(null)} />
         </div>
     );
 }
--- app/frontend/src/components/avatar/Avatar.tsx
+++ app/frontend/src/components/avatar/Avatar.tsx
@@ -1,24 +1,57 @@
-import { Avatar } from "@readyplayerme/visage";
-import hi_animation from "@/assets/Hi_animation.fbx";
+import { Html, OrbitControls, Preload, useAnimations, useGLTF, useFBX } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { AnimationMixer } from "three";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { MyLoader } from "./MyLoader";
+import hi_animation_url from "@/assets/Hi_animation.fbx";
 
-const modelSrc = "https://readyplayerme.github.io/visage/male.glb";
+const Avatar = () => {
+    const [index, setIndex] = useState(1);
+    const { scene } = useGLTF("https://readyplayerme.github.io/visage/male.glb");
+    const { actions, names } = useAnimations([hi_animation_url]);
+    const [isClicked, setIsClicked] = useState(false);
+    const armatureMixerRef = useRef<AnimationMixer | null>(null);
+    useEffect(() => {
+        const armatureMixer = new AnimationMixer(scene);
+        armatureMixerRef.current = armatureMixer;
 
-export function RTAvatar(props: { activeAnimation?: string }) {
-    const { activeAnimation } = props;
-    const onAnimationEnd = () => {
-        console.log("Animation ended");
-    };
+        actions[names[index]]?.reset().fadeIn(0.5).play();
+
+        return () => {
+            actions[names[index]]?.fadeOut(0.5);
+        };
+    }, [index, actions, names, scene]);
+
     return (
-        <Avatar
-            modelSrc={modelSrc}
-            activeAnimation={activeAnimation}
-            animations={{
-                hi: {
-                    source: hi_animation,
-                    repeat: 1
-                }
-            }}
-            onAnimationEnd={onAnimationEnd}
-        />
+        <group>
+            <primitive object={scene} scale={2} position-y={-2} rotation-y={-0.5} position-x={[-1]} />
+
+            <Html position={[-3.7, 0.3, 0]}>
+                <button
+                    className="bg-theme w-[100px] rounded-lg p-2 text-xs text-black duration-500 hover:scale-110 hover:bg-white sm:w-[200px] sm:text-lg"
+                    onClick={() => {
+                        setIndex((index + 1) % names.length);
+                        setIsClicked(!isClicked);
+                    }}
+                >
+                    {isClicked ? "Check out my moves" : "Impress Me!"}
+                </button>
+            </Html>
+        </group>
     );
-}
+};
+
+export const AvatarCanvas = (props: { activeAnimation?: string }) => {
+    const { activeAnimation } = props;
+    return (
+        <Canvas dpr={[0, 2]}>
+            <ambientLight intensity={0.5} />
+            <pointLight position={[1, 1, 1]} />
+            <OrbitControls enabled={true} />
+            <Suspense fallback={<MyLoader />}>
+                <Avatar />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
