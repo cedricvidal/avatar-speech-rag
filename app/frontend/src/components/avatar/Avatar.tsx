ae214dfd5499d50a861fdffb41c35cd40f35ae04
Cedric Vidal <cedric@vidal.biz>
2025-03-12 19:31:17 +0000

    Focus on face and body when animating

--- app/frontend/src/components/avatar/Avatar.tsx
+++ app/frontend/src/components/avatar/Avatar.tsx
@@ -5,6 +5,7 @@ import { MyLoader } from "./MyLoader";
 import myavatar_url from "@/assets/cedric-yellow-pullover-animated.glb";
 import { useTranslation } from "react-i18next";
 import { LoopOnce, LoopRepeat } from "three";
+import { easing } from "maath";
 
 type Props = {
     animation?: string;
@@ -13,34 +14,63 @@ type Props = {
 
 const Avatar = ({ animation }: Props) => {
     const { scene, animations } = useGLTF(myavatar_url);
-    const { actions, names } = useAnimations(animations, scene);
+    const { actions, names, mixer } = useAnimations(animations, scene);
+    const avatarMesh = useRef();
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    useFrame(({ clock }, delta) => {
+        //avatarMesh.current.rotation.y = clock.elapsedTime;
+        //easing.damp3(avatarMesh.current.position, [0, 0, 0], 0.25, delta);
+    });
 
     useEffect(() => {
         console.log("names", names);
 
+        // Event listener for animation completion
+        const onFinished = e => {
+            if (animation && e.action.getClip().name === animation) {
+                console.log("finished");
+                setIsAnimating(false);
+            }
+        };
+
+        mixer.addEventListener("finished", onFinished);
+
         if (animation) {
             console.log("animation changed", animation);
+            setIsAnimating(true);
             actions[animation]?.reset().fadeIn(0.5).setLoop(LoopOnce, 1).play();
+        } else {
+            setIsAnimating(false);
         }
+
         console.log("idle animation");
         actions["idle"]?.reset().fadeIn(0.5).setLoop(LoopRepeat, 100).play();
 
         return () => {
+            mixer.removeEventListener("finished", onFinished);
             if (animation) {
                 console.log("animation fadeOut callback", animation);
                 actions[animation]?.fadeOut(0.5);
             }
         };
-    }, [actions, names, animation]);
+    }, [actions, names, animation, mixer]);
 
-    const focusOnHead = !animation;
+    const focusOnHead = !isAnimating;
     // Adjust position and scale based on focus area
     const scale = focusOnHead ? 8 : 2.5;
     const positionY = focusOnHead ? -12 : -2;
 
+    // If there's no active animation or animation is finished, print "finished"
+    useEffect(() => {
+        if (!isAnimating && !animation) {
+            console.log("finished");
+        }
+    }, [isAnimating, animation]);
+
     return (
         <group>
-            <primitive object={scene} scale={scale} position-y={positionY} rotation-y={0.5} position-x={[0]} />
+            <primitive ref={avatarMesh} object={scene} scale={scale} position-y={positionY} rotation-y={0.5} position-x={[0]} />
         </group>
     );
 };
